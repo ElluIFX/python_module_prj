ab2b59a8461ec6091d82ffdeb0b823745ccdd03f
Ellu <lutaoyu518@gmail.com>
2023-11-29 16:29:04 +0800

    add broken 7796

--- test_lcd.py
+++ test_lcd.py
@@ -5,6 +5,7 @@ import numpy as np
 
 from drivers.interface import register_interface
 from drivers.st7789 import ST7789
+from drivers.st7796 import ST7796
 
 # register_interface("periphery", "spi", "/dev/spidev1.0")
 # register_interface("spidev", "spi", 1, 0)
@@ -23,7 +24,8 @@ register_interface(
 )
 
 # screen = ST7789()
-screen = ST7789(width=240, height=240, offset_left=0, offset_top=0, rotation=270)
+# screen = ST7789(width=240, height=240, offset_left=0, offset_top=0, rotation=270)
+screen = ST7796(width=240, height=240, offset_left=0, offset_top=0)
 print(f"screen initialized with {screen.WIDTH}x{screen.HEIGHT}")
 
 
